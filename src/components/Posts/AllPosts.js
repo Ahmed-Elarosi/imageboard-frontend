cf9a8f532bc83f31df2d67303f3572d6d0a25a3c
Elarosi <cabry95@protonmail,com>
2021-10-10 11:03:31 +0200

    first commit

---
+++ src/components/Posts/AllPosts.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from "./stylesAllPosts";
+import { useSelector } from "react-redux";
+
+import { Grid, CircularProgress } from "@material-ui/core";
+
+const AllPosts = ({ setCurrentId }) => {
+  const classes = useStyles();
+  const posts = useSelector((state) => state.postReducer);
+  console.log("All posts from global state: ", posts);
+
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post setCurrentId={setCurrentId} post={post} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default AllPosts;
